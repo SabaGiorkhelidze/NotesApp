692897e481e03c47a9bdbc50899409863a3d91d9
SavageSB <sabagiorkhelidze@gmail.com>
2023-06-15 12:46:30 +0400

    initial commit

---
+++ src/Components/NewNote.tsx
@@ -0,0 +1,11 @@
+import NoteForm from "./NoteForm";
+const NewNote = () => {
+  return (
+    <div>
+      <h1 className="mb-4">NewNote</h1>
+      <NoteForm />
+    </div>
+  );
+};
+
+export default NewNote;
---
+++ src/Components/NoteForm.tsx
@@ -0,0 +1,75 @@
+import { Col, Form, Row, Stack, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useRef, FormEvent, useState } from "react";
+import CreateaableSelect from "react-select/creatable";
+import { NoteData, Tag } from "../App";
+type NoteFormProps = {
+  onSubmit: (data: NoteData) => void;
+};
+
+const NoteForm = ({ onSubmit }: NoteFormProps) => {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: [],
+    });
+  }
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Stack gap={4}>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control required ref={titleRef} />
+            </Form.Group>
+          </Col>
+
+          <Col>
+            <Form.Group controlId="Tags">
+              <Form.Label>Tags</Form.Label>
+              <CreateaableSelect
+                isMulti
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+
+        <Form.Group controlId="markdown">
+          <Form.Label>Body</Form.Label>
+          <Form.Control required as="textarea" rows={13} ref={markdownRef} />
+        </Form.Group>
+
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
+          <Button type="submit" variant="primary">
+            Save
+          </Button>
+          <Link to="..">
+            <Button type="button" variant="outline-secondary">
+              Cancel
+            </Button>
+          </Link>
+        </Stack>
+      </Stack>
+    </Form>
+  );
+};
+
+export default NoteForm;
